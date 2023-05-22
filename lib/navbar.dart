ab5cbe95059b27bc514801be729ef8e487b0853f
ihsanGhoziZulfikar <ihsanghozizulfikar@gmail.com>
2023-05-22 10:48:05 +0700

    commit1

---
+++ lib/navbar.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:login_page/Aktivitasscreen.dart';
+import 'package:login_page/home.dart';
+import 'package:login_page/syaratdanketentuan_akun.dart';
+import 'package:login_page/umkm.dart';
+
+import 'dana2.dart';
+
+class Navbar extends StatefulWidget {
+  @override
+  _NavbarState createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+  int _currentIndex = 0;
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  List<Widget> listWidget = [
+    Home(title: 'title'),
+    Umkm(title: 'title'),
+    dana2(),
+    AktivitasScreen(),
+    SyaratdanKetentuan(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: listWidget.elementAt(_currentIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        backgroundColor: Colors.blue,
+        onTap: _onTabTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'UMKM',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.money),
+            label: 'Dana',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Akun',
+          ),
+        ],
+      ),
+    );
+  }
+}
