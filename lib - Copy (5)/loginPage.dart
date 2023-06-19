d1a36f7b1d9cac9af0ddd8b162d17f7d41f06a5b
ihsanGhoziZulfikar <ihsanghozizulfikar@gmail.com>
2023-06-19 15:29:57 +0700

    comit44

--- lib - Copy (5)/loginPage.dart
+++ lib - Copy (5)/loginPage.dart
@@ -1,137 +1,12 @@
 import 'package:flutter/material.dart';
+import 'ModelClass/investorModel.dart';
+import 'ModelClass/peminjamModel.dart';
 import 'home.dart';
 import 'registerPage.dart';
 import 'rolePage.dart';
 
 import 'navbar.dart';
 import 'navbar2.dart';
-
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-
-class PeminjamModel {
-  int id;
-  int id_umkm;
-  String nama;
-  String email;
-  String password;
-
-  PeminjamModel({
-    required this.id,
-    required this.id_umkm,
-    required this.nama,
-    required this.email,
-    required this.password,
-  });
-
-  factory PeminjamModel.fromJson(Map<String, dynamic> json) {
-    return PeminjamModel(
-      id: json['id'] ?? '',
-      id_umkm: json['id_umkm'] ?? '',
-      nama: json['nama'] ?? '',
-      email: json['email'] ?? '',
-      password: json['password'] ?? '',
-    );
-  }
-}
-
-class PeminjamCubit extends Cubit<PeminjamModel> {
-  PeminjamCubit()
-      : super(PeminjamModel(
-          id: 0,
-          id_umkm: 0,
-          nama: "",
-          email: "",
-          password: "",
-        ));
-
-  Future<int> fetchData(email, password) async {
-    try {
-      if (email == "" || password == "" || email == "investor") {
-        return -1;
-      }
-      String url = "http://127.0.0.1:8000/peminjam/$email/$password";
-      final response = await http.get(Uri.parse(url));
-      print("hjksdhajkhj jkl");
-      if (response.statusCode == 200) {
-        final jsonData = jsonDecode(response.body);
-        if (jsonData.containsKey("data")) {
-          final data = jsonData['data'] as Map<String, dynamic>;
-          final peminjamModel = PeminjamModel.fromJson(data).id_umkm;
-          return peminjamModel;
-        } else {
-          return -1;
-        }
-      } else {
-        throw Exception('Failed to load data');
-      }
-    } catch (e) {
-      print(e);
-      throw Exception('Failed to load data: $e');
-    }
-  }
-}
-
-class InvestorModel {
-  int id;
-  String nama;
-  String email;
-  String password;
-
-  InvestorModel({
-    required this.id,
-    required this.nama,
-    required this.email,
-    required this.password,
-  });
-
-  factory InvestorModel.fromJson(Map<String, dynamic> json) {
-    return InvestorModel(
-      id: json['id'] ?? '',
-      nama: json['nama'] ?? '',
-      email: json['email'] ?? '',
-      password: json['password'] ?? '',
-    );
-  }
-}
-
-class InvestorCubit extends Cubit<InvestorModel> {
-  InvestorCubit()
-      : super(InvestorModel(
-          id: 0,
-          nama: "",
-          email: "",
-          password: "",
-        ));
-
-  Future<int> fetchData2(email, password) async {
-    try {
-      if (email == "" || password == "") {
-        return -1;
-      }
-      print("hjksdhajkhj jkl 1");
-      String url = "http://127.0.0.1:8000/investor/$email/$password";
-      final response = await http.get(Uri.parse(url));
-      print("hjksdhajkhj jkl 2");
-      if (response.statusCode == 200) {
-        final jsonData = jsonDecode(response.body);
-        if (jsonData.containsKey("data")) {
-          final data = jsonData['data'] as Map<String, dynamic>;
-          final peminjamModel = InvestorModel.fromJson(data).id;
-          return peminjamModel;
-        } else {
-          return -1;
-        }
-      } else {
-        throw Exception('Failed to load data');
-      }
-    } catch (e) {
-      print(e);
-      throw Exception('Failed to load data: $e');
-    }
-  }
-}
 
 class aktivitasUmkm {
   String date;
@@ -246,6 +121,9 @@ class _LoginPageState extends State<LoginPage> {
                             borderRadius: BorderRadius.all(Radius.circular(10)),
                           ),
                           child: TextField(
+                            obscureText: true,
+                            enableSuggestions: false,
+                            autocorrect: false,
                             controller: passController,
                             decoration: InputDecoration(
                               contentPadding:
@@ -279,22 +157,29 @@ class _LoginPageState extends State<LoginPage> {
                             }); //refresh
                             var peminjam =
                                 await peminjamCubit.fetchData(_email, _pass);
-                            if (peminjam != -1) {
+                            if (peminjam != null) {
                               Navigator.of(context).push(
                                 MaterialPageRoute(
                                   builder: (context) {
-                                    return Navbar2(id: peminjam);
+                                    return Navbar2(
+                                      id_peminjam: peminjam.id,
+                                      id_umkm: peminjam.id_umkm,
+                                      id_dana: peminjam.id_dana,
+                                    );
                                   },
                                 ),
                               );
                             } else {
                               var investor =
                                   await investorCubit.fetchData2(_email, _pass);
-                              if (investor != -1) {
+                              if (investor != null) {
                                 Navigator.of(context).push(
                                   MaterialPageRoute(
                                     builder: (context) {
-                                      return Navbar();
+                                      return Navbar(
+                                        id_investor: investor.id,
+                                        id_dana: investor.id_dana,
+                                      );
                                     },
                                   ),
                                 );
